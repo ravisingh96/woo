712e9f5666ea1d7a610abdeb9e4177787954ace6
unknown <ravipratapsingh96@gmail.com>
2020-06-20 16:02:46 +0530

    product service

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -6,29 +6,7 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
-  url = 'https://shop.patnabeats.com';
-  consumerKey = 'ck_77af28ccd6278e16fcaa932675fe6638fa2c0ab8';
-  consumerSecret = 'cs_f9e2bd957526683514666c2e553bff86f9b4874b';
   
-  getProducts() {
-    return `${this.url}/wp-json/wc/v3/products?consumer_key=${
-      this.consumerKey
-    }&consumer_secret=${this.consumerSecret}`;
-  }
-
-  // getProduct(pid) {
-  //   return new Promise(resolve => {
-  //     this.http
-  //       .get(
-  //         `${this.url}/wp-json/wc/v3/products/${pid}?consumer_key=${
-  //           this.consumerKey
-  //         }&consumer_secret=${this.consumerSecret}`
-  //       )
-  //       .subscribe(productData => {
-  //         resolve(productData);
-  //       });
-  //   });
-  // }
 
 
 }
--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -6,4 +6,28 @@ import { Injectable } from '@angular/core';
 export class ProductService {
 
   constructor() { }
+  url = 'https://shop.patnabeats.com';
+  consumerKey = 'ck_77af28ccd6278e16fcaa932675fe6638fa2c0ab8';
+  consumerSecret = 'cs_f9e2bd957526683514666c2e553bff86f9b4874b';
+  
+  getProducts() {
+    return `${this.url}/wp-json/wc/v3/products?consumer_key=${
+      this.consumerKey
+    }&consumer_secret=${this.consumerSecret}`;
+  }
+
+  // getProduct(pid) {
+    //https://shop.patnabeats.com/wp-json/wc/v3/products?consumer_key=ck_77af28ccd6278e16fcaa932675fe6638fa2c0ab8&consumer_secret=cs_f9e2bd957526683514666c2e553bff86f9b4874b
+  //   return new Promise(resolve => {
+  //     this.http
+  //       .get(
+  //         `${this.url}/wp-json/wc/v3/products/${pid}?consumer_key=${
+  //           this.consumerKey
+  //         }&consumer_secret=${this.consumerSecret}`
+  //       )
+  //       .subscribe(productData => {
+  //         resolve(productData);
+  //       });
+  //   });
+  // }
 }
